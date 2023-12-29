315aef2f8dfca9f75ee2a525dc09f08a8ebfa678
lokesh bajracharya <lokesh.bajracharya55@gmail.com>
2023-12-29 22:02:03 +0545

    og graph for communities

---
+++ src/app/communities/[id]/page.tsx
@@ -0,0 +1,71 @@
+import { Metadata, ResolvingMetadata } from "next";
+
+// ui
+export default async function CommunityDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+  const community = await getCommunity(id);
+
+  return (
+    <div>
+      <p>{community.name}</p>
+    </div>
+  );
+}
+
+// get function
+async function getCommunity(id: number | string) {
+  const { data: community } = await fetch(
+    process.env.NEXT_PUBLIC_BASE_API_URL + "/communities/" + id
+  ).then((r) => r.json());
+
+  return community;
+}
+
+// generate static page
+export async function generateStaticParams() {
+  const { data: communities } = await fetch(
+    process.env.NEXT_PUBLIC_BASE_API_URL + "/communities/"
+  ).then((res) => res.json());
+
+  return communities.map(({ name }: any) => ({
+    id: name,
+  }));
+}
+
+// generate meta data
+export async function generateMetadata(
+  { params }: any,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { id } = params;
+
+  const { data: community } = await fetch(
+    process.env.NEXT_PUBLIC_BASE_API_URL + "/communities/" + id
+  ).then((r) => r.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  function generateImage() {
+    return (
+      process.env.NEXT_PUBLIC_IMAGE_API_URL +
+      "/media/lg/" +
+      community.image +
+      ".webp"
+    );
+  }
+
+  return {
+    applicationName: "Lyfgram",
+    title: community.name,
+    openGraph: {
+      title: community.name,
+      description: community.description,
+      images: [generateImage(), ...previousImages],
+    },
+  };
+}
